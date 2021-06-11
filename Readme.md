f0370d31e6bf2ac6df30ed4bcfec05d162661997
maggie sun <maggie20080103@gmail.com>
2021-06-11 01:03:16 -0400

    Handle Navigation Tab and icon

--- Readme.md
+++ Readme.md
@@ -14,8 +14,10 @@ Website Demo✨: https://105306005.github.io/Quote-practice/
 Project is created with:
 
 - Expo, React Native, Redux , Firebase
-- Routes: React Navigation https://reactnavigation.org/docs/getting-started
 - Firebase: store user data with Firestore Database
+- Routes: React Navigation https://reactnavigation.org/docs/getting-started - bottom-tabs
+- Navigation icons: react-native-vector-icons (MaterialCommunityIcons)
+
 
 <h3 align="left">Languages and Tools:</h3>
 <p align="left"> <a href="https://firebase.google.com/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://reactnative.dev/" target="_blank"> <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40"/> </a> </p>
--- components/Main.js
+++ components/Main.js
@@ -1,24 +1,59 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchUser } from "../redux/actions/index";
+
+import FeedScreen from "./main/Feed";
+import AddScreen from "./main/Add";
+import ProfileScreen from "./main/Profile";
+
+const Tab = createBottomTabNavigator();
 
 export class Main extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
   render() {
-    const { currentUser } = this.props;
-    console.log(currentUser);
-    if (currentUser === undefined) {
-      return <View></View>;
-    }
     return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>{currentUser.name} is log in</Text>
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Feed"
+          component={FeedScreen}
+          options={{
+            // tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={AddScreen}
+          options={{
+            // tabBarLabel: "Add",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="plus-box" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            // tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account-circle"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     );
   }
 }
