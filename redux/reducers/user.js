6dfebf5b86566b1fb40b954ccaad81cba30aa116
maggie sun <maggie20080103@gmail.com>
2021-06-16 22:23:50 -0400

    following system with firestore

--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,8 +1,13 @@
-import { USER_POSTS_STATE_CHANGE, USER_STATE_CHANGE } from "../constants";
+import {
+  USER_POSTS_STATE_CHANGE,
+  USER_STATE_CHANGE,
+  USER_FOLLOWING_STATE_CHANGE,
+} from "../constants";
 
 const initialState = {
   currentUser: null,
   posts: [],
+  following: [],
 };
 
 export const user = (state = initialState, action) => {
@@ -11,6 +16,8 @@ export const user = (state = initialState, action) => {
       return { ...state, currentUser: action.currentUser };
     case USER_POSTS_STATE_CHANGE:
       return { ...state, posts: action.posts };
+    case USER_FOLLOWING_STATE_CHANGE:
+      return { ...state, following: action.following };
     default:
       return state;
   }
